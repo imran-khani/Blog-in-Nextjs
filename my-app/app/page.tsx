b1709aeaf8d2642e2fe82da7e8d69eb348cae363
imran khan <imranloveh01@gmail.com>
2023-12-26 01:09:04 +0500

    Fix formatting and syntax errors in page.tsx

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -8,9 +8,9 @@ import Link from "next/link";
 
 const getData = async () => {
   const query = `
-    *[_type=='heroImage']{
+    *[_type=="heroImage"]{
         heroImage,
-      }[0]`;
+      }[0]`
   const data = await client.fetch(query);
   return data;
 };
@@ -19,7 +19,7 @@ const Home = async () => {
   return (
     <section className="text-gray-600 body-font">
       <MotionDiv
-      key={data.heroImage}
+        key={data.heroImage}
         animation={{
           initial: { opacity: 0 },
           animate: { opacity: 1, scale: 1.0, z: 0 },
@@ -27,23 +27,24 @@ const Home = async () => {
           transition: { duration: 0.5 },
           exit: { opacity: 0 },
         }}
-        className="mx-auto flex  py-5 md:flex-row flex-col items-center "
+        className="mx-auto flex py-5 md:flex-row flex-col items-center "
       >
-        <MotionDiv className=" lg:flex-grow md:w-1/2 py-12  md:pr-16 flex flex-col md:items-start  mb-16 md:mb-0  ">
+        <MotionDiv className="lg:flex-grow md:w-1/2 py-12  md:pr-16 flex flex-col md:items-start  mb-16 md:mb-0  ">
           <h1 className="text-2xl sm:text-4xl  leading-relaxed  mb-4 font-medium text-gray-900 dark:text-white ">
             Frontend Developer Crafting <br /> Tomorrow Digital Experience
           </h1>
           <p className="mb-8 leading-relaxed dark:text-white ">
-            Hey there! Im Imran Khan, a frontend developer specializing in
-            React and Next.js. For two years, Ive been weaving creativity into
-            code to build captivating digital experiences. Join me on this
-            journey as we shape the future of the web, pixel by pixel.
+            Hey there! Im Imran Khan, a frontend developer specializing in React
+            and Next.js. For two years, Ive been weaving creativity into code to
+            build captivating digital experiences. Join me on this journey as we
+            shape the future of the web, pixel by pixel.
           </p>
           <div className="flex justify-center">
-            <Link href={'blog'}>
-           <Button className="inline-flex text-white bg-primary/90  border-0 py-2 px-6 focus:outline-none hover:bg-primary  rounded text-lg">
-              Read Blog
-            </Button> </Link>
+            <Link href={"blog"}>
+              <Button className="inline-flex text-white bg-primary/90  border-0 py-2 px-6 focus:outline-none hover:bg-primary  rounded text-lg">
+                Read Blog
+              </Button>{" "}
+            </Link>
             <Button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">
               Download CV
             </Button>
