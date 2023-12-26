26b33d542b91125602ad4e2117dfe2ea24533d71
imran khan <imranloveh01@gmail.com>
2023-12-26 14:17:06 +0500

    Fix import paths and formatting in sanity.ts and tsconfig.json

--- app/(sanity)/sanity.ts
+++ app/(sanity)/sanity.ts
@@ -1,10 +1,10 @@
-import { conf } from '@/conf/conf'
-import { createClient } from 'next-sanity'
-import imageUrlBuilder from '@sanity/image-url'
+import { conf } from '../../conf/conf';
+import { createClient } from 'next-sanity';
+import imageUrlBuilder from '@sanity/image-url';
 
-const projectId = conf.sanityProjectId
-const dataset = conf.sanityDataset
-const apiVersion = conf.sanityApiVersion
+const projectId = conf.sanityProjectId;
+const dataset = conf.sanityDataset;
+const apiVersion = conf.sanityApiVersion;
 
 export const client = createClient({
     projectId,
